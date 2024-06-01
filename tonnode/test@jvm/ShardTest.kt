e6e3d87f9855c16999a1bc53d75d9cc3696d009a
Andrey Pfau <andreypfau@ton.org>
2024-06-01 20:42:14 +0400

    add tests for raw Long in ShardId

--- tonnode/test@jvm/ShardTest.kt
+++ tonnode/test@jvm/ShardTest.kt
@@ -8,18 +8,21 @@ class ShardTest {
     @Test
     fun testShardId() {
         val shard_4000000000000000 = ShardId(shardPrefix = 0u, length = 1)
+        assertEquals(shard_4000000000000000, ShardId(0x4000000000000000uL))
         assertEquals(shard_4000000000000000, ShardId("4000000000000000"))
         assertEquals(shard_4000000000000000.toString(), "4000000000000000")
         assertEquals(shard_4000000000000000.length(), 1)
         assertEquals(shard_4000000000000000.parent(), ShardId.ROOT)
 
         val shard_c000000000000000 = ShardId(shardPrefix = 1u, length = 1)
+        assertEquals(shard_c000000000000000, ShardId(0xc000000000000000uL))
         assertEquals(shard_c000000000000000, ShardId("c000000000000000"))
         assertEquals(shard_c000000000000000.toString(), "c000000000000000")
         assertEquals(shard_c000000000000000.length(), 1)
         assertEquals(shard_c000000000000000.parent(), ShardId.ROOT)
 
         assertEquals(ShardId.ROOT, ShardId("8000000000000000"))
+        assertEquals(ShardId.ROOT, ShardId(0x8000000000000000uL))
         assertEquals(ShardId.ROOT.toString(), "8000000000000000")
         assertEquals(ShardId.ROOT.length(), 0)
         assertEquals(ShardId.ROOT.childLeft(), shard_4000000000000000)
